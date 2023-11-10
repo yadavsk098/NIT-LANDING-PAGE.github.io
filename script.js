5342ebd806551c4e9f4bc96de422fd6dc012d147
yadavsk098 <yadavsk098@gmail.com>
2023-11-10 22:46:07 +0530

    images added

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var swiper = new Swiper(".mySwiper", {
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
+        loop: true
     },
     pagination: {
         el: ".swiper-pagination",
